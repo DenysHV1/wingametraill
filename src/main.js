e29a9efbdbd56dc604646fd4259a9cd2700d6ed1
DenVik <denyagarkusha2@gmail.com>
2025-01-16 22:55:49 +0100

    start have done

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { header } from "./js/header";
+import { faq } from "./js/faq";
+import { gallerySlider } from "./js/gallerySlider";
+import { reviewsSlider } from "./js/reviewsSlider";
+
+header()
+faq()
+gallerySlider()
+reviewsSlider()
