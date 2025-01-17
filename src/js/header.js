ccc85ea8b64dba9d1b73af1de3e247d265fa2b81
DenVik <denyagarkusha2@gmail.com>
2025-01-17 12:01:29 +0100

    hero, header have done

--- src/js/header.js
+++ src/js/header.js
@@ -1,3 +1,20 @@
 export const header = () => {
-  
-}
+  const openMenuBtn = document.querySelector('.burger_menu-js');
+  const closeMenuBtn = document.querySelector('.close-menu-js');
+  const menu = document.querySelector('.mobile-nav-js');
+  const menuItems = document.querySelectorAll('.mobile-menu-item-js');
+
+  openMenuBtn.addEventListener('click', () => {
+    menu.classList.add('is-open');
+  });
+
+  closeMenuBtn.addEventListener('click', () => {
+    menu.classList.remove('is-open');
+  });
+
+  menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+      menu.classList.remove('is-open');
+    });
+  });
+};
