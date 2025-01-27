09b8e53d57ca7b8504624739763d3cd9a1355a27
DenVik <denyagarkusha2@gmail.com>
2025-01-27 19:33:02 +0100

    fix for button

--- src/js/scrollUp.js
+++ src/js/scrollUp.js
@@ -4,8 +4,4 @@ export const scrollUp = () => {
 window.addEventListener('scroll', function(){
 	scrollUp.classList.toggle("is-open-scroll", window.scrollY>500)
 })
-
-// window.addEventListener('scroll', function(){
-// 	scrollUp.classList.toggle("scroll-color", window.scrollY>5500)
-// })
 }
